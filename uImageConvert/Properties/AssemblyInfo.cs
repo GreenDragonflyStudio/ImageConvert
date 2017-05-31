295847bf84887ded0c27ddfce8a9c0fa702d0d24
Muhammad Fahmi Rasyid <ufarasfa@gmail.com>
2017-06-01 02:04:37 +0700

    Update Project Version

--- uImageConvert/Properties/AssemblyInfo.cs
+++ uImageConvert/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("uImageConvert")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Universal Image Converter")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rasyid Fahmi")]
 [assembly: AssemblyProduct("uImageConvert")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Rasyid Muhammad Fahmi 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: Guid("1C426DD3-41DE-4746-B05B-DC22A706075F")]
+
